67456236f8fdca077c038bc220cbcd824d04518b
SwientekMatej <SwientekMatej@sssvt.cz>
2026-02-16 09:07:27 +0100

    Started work on Config List Window

---
+++ zalohovaci_system_editor/Components/DropdownList.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using zalohovaci_system_editor.Model;
+
+namespace zalohovaci_system_editor.Components
+{
+    public class DropdownList : IComponent
+    {
+        public Dictionary<ConsoleKey, Action> KeyInputs { get; set; } = new Dictionary<ConsoleKey, Action>()
+        { 
+            { ConsoleKey.UpArrow, () => { selectedLine = Math.Min(++selectedLine, Values.Count - 1); } },
+            { ConsoleKey.DownArrow, () => { selectedLine = Math.Max(--selectedLine, 0);; } }
+        };
+
+        public static List<string> Values { get; set; }
+        private static int selectedLine = 0;
+
+        public void Draw()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void HandleKey(ConsoleKeyInfo keyInfo)
+        {
+            if (KeyInputs.ContainsKey(keyInfo.Key))
+            {
+                KeyInputs[keyInfo.Key].Invoke();
+            }
+        }
+    }
+}
--- zalohovaci_system_editor/Windows/ConfigListWindow.cs
+++ zalohovaci_system_editor/Windows/ConfigListWindow.cs
@@ -13,11 +13,7 @@ namespace zalohovaci_system_editor.Windows
 
         public override void Draw()
         {
-            Console.Write("Test Levé okno");
-            Console.SetCursorPosition(2, Console.CursorTop + 2);
-            Console.Write("Test Levé okno řádek 1");
-            Console.SetCursorPosition(2, Console.CursorTop + 1);
-            Console.Write("Test Levé okno řádek 2");
+
         }
 
         public override void HandleKey(ConsoleKeyInfo keyInfo)
